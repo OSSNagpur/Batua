817ae6ec99a801fdcebf56204e72f59829a1c3a9
Kshitij Shambharkar <kshitijshambharkar07@gmail.com>
2025-10-15 03:32:55 +0530

    setup shadcn-ui

--- batua-ui/src/App.tsx
+++ batua-ui/src/App.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button"
 function App() {
 
   return (
-   <div className='flex justify-center items-center h-screen'>
+   <div className='flex flex-col justify-center items-center h-screen gap-10'>
     <h1 className='text-3xl font-bold underline'>
       Hello
     </h1>
